0c891d93091c6f14bf91b956618d017cefe887ce
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2023-09-23 16:44:13 +0200

    works for now

--- background.js
+++ background.js
@@ -27,11 +27,24 @@ chrome.action.onClicked.addListener(async (tab) => {
 });
 
 chrome.runtime.onMessage.addListener(
-  function (request, sender, callback) {
+  function (request, sender, callback) 
+  {
+    console.log(request);
+    console.log(sender);
     if (request.action == 'hydrusAPI')
     {
-      console.log(request);
-      callback('Success!');
+      fetch(api + request.method, {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          'Hydrus-Client-API-Access-Key': key
+        },
+        body: request.data
+      }).then((response) => {
+        console.log(response);
+        callback(response.status);
+      });
     }
+  return true; // return true to indicate you want to send a response asynchronously 
   }
 );
