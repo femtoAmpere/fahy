32ee643bce5a15841c1e17ef65e45e1c3f2785ae
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2023-09-23 15:55:47 +0200

    extract infos from FA classic page

--- background.js
+++ background.js
@@ -1,17 +1,37 @@
 
-function badgeText(txt)
-{
-  chrome.action.setBadgeText({
-    text: txt
-  });
-}
+const api = 'http://127.0.0.1:45869'
+const key = '0a508929a717adc08046b6254afbb8c866cd4efc831ba594953abb8053000683';
 
 // When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
-  //if (!tab.url.toLowerCase().startsWith(urlEnabled.toLowerCase())) {
-  //if (tab.url.toLowerCase().startsWith(urlEnabled.toLowerCase()))
-  //{
-    badgeText('YES');
-    console.log(tab.url);
- //}
+  console.log('clicked');
+
+  if (!tab.url.includes("furaffinity.net/view/"))
+  {
+    badgeText('ERR');
+    return;
+  }
+  badgeText('WORK');
+  console.log(tab.document);
+
+  tags = ["url:" + tab.url];
+  executeScript({
+    code: "document.documentElement.innerHTML" // or 'file: "getPagesSource.js"'
+  }, function (result) {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError.message);
+    } else {
+      console.log(result)
+    }
+  });
 });
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, callback) {
+    if (request.action == 'hydrusAPI')
+    {
+      console.log(request);
+      callback('Success!');
+    }
+  }
+);
--- content.js
+++ content.js
@@ -1,5 +1,68 @@
-chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-    if (change.url) {
-        console.log(change.url);
+
+function hyGetTags() {
+    var tags = [];
+    tags.push('site:furaffinity');
+
+    const pageUrl = window.location.href.endsWith('/') ? window.location.href : window.location.href + '/';
+    tags.push('url:' + pageUrl);
+    tags.push('id:' + pageUrl.split('/').at(-2));
+
+    const downloadUrl = hyGetUrl();
+    tags.push('url:' + downloadUrl);
+
+    const filename = downloadUrl.split(/(\\|\/)/g).pop();
+    tags.push('filename:' + filename);
+    tags.push('extension:' + filename.split('.').at(-1));
+    tags.push('faid:' + filename.split('.').at(0));
+
+    const submissionInfos = document.getElementsByClassName('classic-submission-title information')[0];
+    tags.push('title:' + submissionInfos.firstElementChild.innerText);
+    tags.push('creator:' + submissionInfos.lastElementChild.innerText);
+    tags.push('url:https://furaffinity.net/user/' + submissionInfos.lastElementChild.innerText + '/');
+
+    const keywords = document.getElementById('keywords');
+    if (keywords) {
+        const htmlTags = keywords.getElementsByTagName('a');
+        for (const a in htmlTags) {
+            var tag = htmlTags[a].innerText;
+            if (tag) { tags.push(tag); }
+        }
     }
-});
+
+
+    console.log('tags: ' + tags);
+    return tags;
+}
+
+function hyGetUrl() {
+    const url = document.getElementById('submissionImg').src;
+    console.log('url: ' + url);
+    return url;
+}
+
+function hyPost() {
+    chrome.runtime.sendMessage({
+        action: 'hydrusAPI',
+        method: '/add_urls/add_url',
+        data: JSON.stringify({
+            url: hyGetUrl(),
+            //destination_page_key: 1,
+            destination_page_name: 'furaffinity',
+            show_destination_page: false,
+            //service_keys_to_additional_tags
+            filterable_tags: hyGetTags()
+        })
+    }, function (responseText) {
+        console.log(responseText);
+        
+        hyButton.innerHTML = '<h2>[ ' + responseText + ' ]</h2>';
+    });
+
+    hyButton.removeEventListener('click', hyPost, true);
+}
+
+const hyButton = document.createElement("p");
+const insertButton = document.getElementById('page-submission');
+const inserted = insertButton.prepend(hyButton);
+hyButton.innerHTML = '<h2>[ hydrus ]</h2>';
+hyButton.addEventListener('click', hyPost, true);
