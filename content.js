ba513c90a48c91cae9b65ed2afedac397cb60a3a
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2023-10-16 02:06:26 +0200

    added fa gallery folders to scraped keywords

--- content.js
+++ content.js
@@ -22,29 +22,38 @@ function hyGetTags() {
     tags.push('creator:' + submissionInfos.lastElementChild.innerText);
     tags.push('url:https://furaffinity.net/user/' + submissionInfos.lastElementChild.innerText + '/');
 
+    const foldersElement = document.getElementsByClassName('clearfloat')
+    if (foldersElement.length > 0) {
+        const folders = foldersElement[0].getElementsByTagName('li');
+        for (const a of folders) {
+            tags.push('url:' + a.firstElementChild.href);
+            tags.push('folder:' + a.innerText);
+        }
+    }
+
     const statsContainer = document.getElementsByClassName('alt1 stats-container')[0];
     const stats = statsContainer.getElementsByTagName('b');
-    for (const a in stats)
+    for (const a of stats)
     {
-        var thisText = stats[a].innerText;
+        var thisText = a.innerText;
         switch (thisText) {
             case undefined:
                 break;
             case 'Submission Information:':
                 break;
             case 'Posted:':
-                tags.push(thisText + stats[a].nextElementSibling.title);
-                tags.push(thisText + stats[a].nextElementSibling.innerText);
+                tags.push(thisText + a.nextElementSibling.title);
+                tags.push(thisText + a.nextElementSibling.innerText);
                 break;
             case 'Image Specifications:':
                 break;
             case 'Favorites:':
-                tags.push(thisText + stats[a].nextElementSibling.innerText);
+                tags.push(thisText + a.nextElementSibling.innerText);
                 break;
             case 'Keywords:':
                 break;
             default:
-                tags.push(thisText + stats[a].nextSibling.textContent);
+                tags.push(thisText + a.nextSibling.textContent);
         }
     }
 
@@ -68,8 +77,8 @@ function hyGetTags() {
     const keywords = document.getElementById('keywords');
     if (keywords) {
         const htmlTags = keywords.getElementsByTagName('a');
-        for (const a in htmlTags) {
-            var tag = htmlTags[a].innerText;
+        for (const a of htmlTags) {
+            var tag = a.innerText;
             if (tag) { tags.push(tag); }
         }
     }
